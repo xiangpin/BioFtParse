ad44a96172b74d88a548adfc9db61e162514886f
xiangpin <xshuangbin@163.com>
2020-12-09 17:24:49 +0800

    add examples

---
+++ examples/example1/run_example1.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+parse_genbank.py GCA_900604365.1_16118_1_1_genomic.gbff.gz -s GenomicGb.fasta -t GenomicGbSeq.annot -c GenomicGbCds.annot
---
+++ examples/example2/run_example2.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+parse_genbank.py ./GCA_900604365.1_16118_1_1_protein.gpff.gz -s ProteinGb.fasta -t ProteinGbSeq.annot -c ProteinGbCds.annot
